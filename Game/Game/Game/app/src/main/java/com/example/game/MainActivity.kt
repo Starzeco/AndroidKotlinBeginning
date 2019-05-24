946f078b7046c738ead2a33c31e4333999dd2d88
Starzec <marcel.k@onet.eu>
2019-05-24 22:29:52 +0200

    Repaired movement

--- Game/Game/Game/app/src/main/java/com/example/game/MainActivity.kt
+++ Game/Game/Game/app/src/main/java/com/example/game/MainActivity.kt
@@ -54,6 +54,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
 
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_GAME)
+        sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_GAME)
+
         springAnimX = findViewById<View>(R.id.imageView5).let { img ->
             SpringAnimation(img, DynamicAnimation.TRANSLATION_X, currentX)
         }
@@ -67,8 +70,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_GAME)
-        sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_GAME)
     }
 
     override fun onPause() {
@@ -77,78 +78,84 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        if(event!!.sensor.type == Sensor.TYPE_ACCELEROMETER) {
-            currentX += event.values[0]
-            currentY += event.values[1]
-            if (currentX < 0 || currentX > width || currentY < 0 || currentY > height) {
-                counter++
-                sound.start()
-                springAnimY.animateToFinalPosition(0f)
-                springAnimX.animateToFinalPosition(0f)
-            }else{
-                val forceX = SpringForce(-currentX)
-                    .setDampingRatio(SpringForce.DAMPING_RATIO_NO_BOUNCY)
-                    .setStiffness(SpringForce.STIFFNESS_LOW)
-                springAnimX.spring = forceX
-                springAnimX.start()
+        if(event != null) {
+            if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                currentX += event.values[0]*3
+                currentY += event.values[1]*3
+                if (currentX < -width / 2 || currentX > width / 2 || currentY < -height / 2 || currentY > height / 2) {
+                    counter++
+                    sound.start()
+                    currentX = 0f
+                    currentY = 0f
+                    springAnimY.animateToFinalPosition(0f)
+                    springAnimX.animateToFinalPosition(0f)
+                    springAnimX.start()
+                    springAnimY.start()
+                } else {
+                    val forceX = SpringForce(-currentX)
+                        .setDampingRatio(SpringForce.DAMPING_RATIO_NO_BOUNCY)
+                        .setStiffness(SpringForce.STIFFNESS_MEDIUM)
+                    springAnimX.spring = forceX
+                    springAnimX.start()
 
-                val forceY = SpringForce(currentY)
-                    .setDampingRatio(SpringForce.DAMPING_RATIO_NO_BOUNCY)
-                    .setStiffness(SpringForce.STIFFNESS_LOW)
-                springAnimY.spring = forceY
-                springAnimY.start()
-            }
-        }else{
-            if(event.values[0] < 500){
-                if(counter > 100){
-                    counter = 0
-                    val random = Random.nextInt(10)
-                    when(random){
-                        0 -> {
-                            imageView5.setImageResource(R.mipmap.custom_caterpie_foreground)
-                            sound = MediaPlayer.create(this, R.raw.caterpie)
-                        }
-                        1 -> {
-                            imageView5.setImageResource(R.mipmap.custom_charmander_foreground)
-                            sound = MediaPlayer.create(this, R.raw.charmander)
-                        }
-                        2 -> {
-                            imageView5.setImageResource(R.mipmap.custom_eevee_foreground)
-                            sound = MediaPlayer.create(this, R.raw.eevee)
-                        }
-                        3 -> {
-                            imageView5.setImageResource(R.mipmap.custom_egg_foreground)
-                            sound = MediaPlayer.create(this, R.raw.egg)
-                        }
-                        4 -> {
-                            imageView5.setImageResource(R.mipmap.custom_jigg_foreground)
-                            sound = MediaPlayer.create(this, R.raw.jigg)
-                        }
-                        5 -> {
-                            imageView5.setImageResource(R.mipmap.custom_mankey_foreground)
-                            sound = MediaPlayer.create(this, R.raw.mankey)
-                        }
-                        6 -> {
-                            imageView5.setImageResource(R.mipmap.custom_meowth_foreground)
-                            sound = MediaPlayer.create(this, R.raw.meowth)
-                        }
-                        7 -> {
-                            imageView5.setImageResource(R.mipmap.custom_pidgey_foreground)
-                            sound = MediaPlayer.create(this, R.raw.pidgey)
-                        }
-                        8 -> {
-                            imageView5.setImageResource(R.mipmap.custom_pikachu_foreground)
-                            sound = MediaPlayer.create(this, R.raw.pikachu)
-                        }
-                        9 -> {
-                            imageView5.setImageResource(R.mipmap.custom_psy_foreground)
-                            sound = MediaPlayer.create(this, R.raw.psyduck)
-                        }
-                    }
-                }else{
-                    Toast.makeText(this, "You have to try more", Toast.LENGTH_SHORT).show()
+                    val forceY = SpringForce(currentY)
+                        .setDampingRatio(SpringForce.DAMPING_RATIO_NO_BOUNCY)
+                        .setStiffness(SpringForce.STIFFNESS_MEDIUM)
+                    springAnimY.spring = forceY
+                    springAnimY.start()
                 }
+            } else {
+                if (event.values[0] < 500) {
+                    if (counter > Random.nextInt(10, 30)) {
+                        counter = 0
+                        val random = Random.nextInt(10)
+                        when (random) {
+                            0 -> {
+                                imageView5.setImageResource(R.mipmap.custom_caterpie_foreground)
+                                sound = MediaPlayer.create(this, R.raw.caterpie)
+                            }
+                            1 -> {
+                                imageView5.setImageResource(R.mipmap.custom_charmander_foreground)
+                                sound = MediaPlayer.create(this, R.raw.charmander)
+                            }
+                            2 -> {
+                                imageView5.setImageResource(R.mipmap.custom_eevee_foreground)
+                                sound = MediaPlayer.create(this, R.raw.eevee)
+                            }
+                            3 -> {
+                                imageView5.setImageResource(R.mipmap.custom_egg_foreground)
+                                sound = MediaPlayer.create(this, R.raw.egg)
+                            }
+                            4 -> {
+                                imageView5.setImageResource(R.mipmap.custom_jigg_foreground)
+                                sound = MediaPlayer.create(this, R.raw.jigg)
+                            }
+                            5 -> {
+                                imageView5.setImageResource(R.mipmap.custom_mankey_foreground)
+                                sound = MediaPlayer.create(this, R.raw.mankey)
+                            }
+                            6 -> {
+                                imageView5.setImageResource(R.mipmap.custom_meowth_foreground)
+                                sound = MediaPlayer.create(this, R.raw.meowth)
+                            }
+                            7 -> {
+                                imageView5.setImageResource(R.mipmap.custom_pidgey_foreground)
+                                sound = MediaPlayer.create(this, R.raw.pidgey)
+                            }
+                            8 -> {
+                                imageView5.setImageResource(R.mipmap.custom_pikachu_foreground)
+                                sound = MediaPlayer.create(this, R.raw.pikachu)
+                            }
+                            9 -> {
+                                imageView5.setImageResource(R.mipmap.custom_psy_foreground)
+                                sound = MediaPlayer.create(this, R.raw.psyduck)
+                            }
+                        }
+                    } else {
+                        Toast.makeText(this, "You have to try more", Toast.LENGTH_SHORT).show()
+                    }
 
+                }
             }
         }
     }
